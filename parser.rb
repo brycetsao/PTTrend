d2a92a29bd5d9d0a27e829fbb379f02def76e046
tsaodingtw <tsaodingtw@gmail.com>
2016-04-17 23:01:28 +0800

    parsing comments

--- parser.rb
+++ parser.rb
@@ -1,17 +1,29 @@
 require 'mechanize'
-require 'rmmseg'
 
 agent = Mechanize.new
 payload = { 'from' => '/bbs/Gossiping/index.html', 'yes' => 'yes' }
 agent.post 'https://www.ptt.cc/ask/over18', payload
 p = agent.page
-links = p.css('.r-list-container a')[1, 0]
+links = p.css('.r-list-container a')
 
+# db = SQLite3::Database.open 'ptt.db'
+
+# current_year = 2016
+# base_url = 'https://www.ptt.cc'
 
 links.each do |l|
 	p = agent.get l['href']
 	m = p.at_css('#main-content')
+
+	comments = p.css('.push').map &:text
+	# puts comments
+
 	m.search('.//div').remove
-	# puts m.text
-	puts 'a'
+	content = m.text[/(.|\n)*--\n/]
+	# puts content
+
+
+	# db.execute <<-SQL
+	# 	insert into article values('title', 'board', 'author', 'date', m.text, comment)
+	# SQL
 end
