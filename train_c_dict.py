3748d5a7a6a581ac70b26262251a98a79a4138fd
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-06-17 15:39:04 +0800

    Add Chinese Dict

---
+++ train_c_dict.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import numpy as np
+from feature import Counter, Feature
+import sqlite3
+
+
+db = sqlite3.connect('ptt.db')
+cur = db.execute('select * from articles')
+f = Feature()
+a = 0
+for i in cur:
+    a += 1
+    try:
+        f.features(i[5])
+    except:
+        print(i[5])
+    print(a)
+
+f.store()
