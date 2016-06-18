7743c8ec485c2a96ba222ca00cc6debe140ac376
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-06-18 17:27:49 +0800

    Add Saver

--- tf.py
+++ tf.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 import numpy as np
 from feature import Feature
 import sqlite3
+import pickle
 
 db = sqlite3.connect('ptt.db')
-cur = db.execute('SELECT * FROM ARTICLES LIMIT 100')
+cur = db.execute('SELECT * FROM ARTICLES LIMIT 1000')
 
 
 # Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
@@ -20,27 +21,30 @@ x = tf.placeholder(tf.float32, shape=(300000, 1))
 # Try to find values for W and b that compute y_data = W * x_data + b
 # (We know that W should be 0.1 and b 0.3, but Tensorflow will
 # figure that out for us.)
-W = tf.Variable(tf.random_uniform([300000, 1], -1.0, 1.0))
-b = tf.Variable(tf.zeros([1]))
-y = tf.matmul(W, x, transpose_a=True) + b
+W = tf.Variable(tf.random_uniform([300000, 1], -1.0, 1.0), name="Weigh")
+b = tf.Variable(tf.zeros([1]), name="Bias")
+y = tf.add(tf.matmul(W, x, transpose_a=True), b)
 
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.00000005)
+optimizer = tf.train.GradientDescentOptimizer(0.0000005)
 train = optimizer.minimize(loss)
 
 # Before starting, initialize the variables.  We will 'run' this first.
 init = tf.initialize_all_variables()
 
 # Launch the graph.
+saver = tf.train.Saver([W, b])
 sess = tf.Session()
 sess.run(init)
 
 # Fit the line.
-for step in range(201):
+for step in range(20001):
     for data in x_data:
         sess.run(train, feed_dict={x: data})
-    #if step % 20 == 0:
-    print(step, sess.run(W), sess.run(b))
+    if step % 20 == 0:
+        print(step, sess.run(W), sess.run(b))
+    if step % 1000 == 0:
+        # Append the step number to the checkpoint name:
+        saver.save(sess, 'my-model', global_step=step)
 
-# Learns best fit is W: [0.1], b: [0.3]
