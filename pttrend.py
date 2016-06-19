1023031b71993a33b49036480ed3c064a254209f
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-06-19 17:27:57 +0800

    Tensorflow in pttrend.py

--- pttrend.py
+++ pttrend.py
@@ -14,3 +14,12 @@ post_feature = [f.features(sys.argv[1])]
 print(clf.predict(post_feature)[0])
 print(clf.predict_proba(post_feature)[0])
 
+sess = tf.Session()
+W = tf.Variable(tf.random_uniform([300000, 1], -1.0, 1.0), name="Weigh")
+b = tf.Variable(tf.zeros([1]), name="Bias")
+saver = tf.train.Saver()
+saver.restore(sess, 'my-model-20000')
+x = f.features_for_tensorflow(sys.argv[1])
+y = tf.add(tf.matmul(W, x, transpose_a=True), b)
+comment = sess.run(y)
+print(comment[0][0])
