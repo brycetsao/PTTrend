df2d89dac6c8b765bd6d54e01f4d4057b99ae7e8
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-06-19 20:15:40 +0800

    paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, :only => [ :show, :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all
+    @posts = Post.order('updated_at DESC').page(params[:page]).per(10)
   end
 
   def show
